9a34198e8bc56fdf47c37c23af8aac4b7f6c6698
Mazin-Shamshad <mskes005@gmail.com>
2024-08-15 14:18:39 +0530

    lcs_dp

---
+++ lcs.cpp
@@ -0,0 +1,28 @@
+// function to find longest common subsequence
+// valid for both strings and vectors/arrays
+class Solution
+{
+public:
+    // Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string str1, string str2)
+    {
+        // your code here
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (str1[i - 1] == str2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
