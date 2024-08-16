f0a999dc77c73122cf302bae9e74c9dde0e19537
Mazin Shamshad <mskes005@gmail.com>
2024-08-16 19:17:23 +0530

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,4 +1,43 @@
 #define ll long long
+#define int long long
+
+void buildSegmentTree(int i, int l, int r, vector<int>& segmentTree, int arr[]) {
+    if (l == r) {
+        segmentTree[i] = arr[l];
+        return;
+    }
+    int mid = l + (r - l) / 2;
+    buildSegmentTree(2 * i + 1, l, mid, segmentTree, arr);
+    buildSegmentTree(2 * i + 2, mid + 1, r, segmentTree, arr);
+    segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+}
+ 
+int querySegmentTree(int start, int end, int i, int l, int r, vector<int>& segmentTree) {
+    if (l > end || start > r) {
+        return 0;
+    }
+    if (l >= start && r <= end) {
+        return segmentTree[i];
+    }
+    int mid = l + (r - l) / 2;
+    return querySegmentTree(start, end, 2 * i + 1, l, mid, segmentTree) +
+           querySegmentTree(start, end, 2 * i + 2, mid + 1, r, segmentTree);
+}
+ 
+void updateSegmentTree(int i, int l, int r, int idx, int value, vector<int>& segmentTree) {
+    if (l == r) {
+        segmentTree[i] += value;  // Increment the value
+        return;
+    }
+    int mid = l + (r - l) / 2;
+    if (idx <= mid) {
+        updateSegmentTree(2 * i + 1, l, mid, idx, value, segmentTree);
+    } else {
+        updateSegmentTree(2 * i + 2, mid + 1, r, idx, value, segmentTree);
+    }
+    segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+}
+
 
 class RangeSumQuery
 {
@@ -59,6 +98,9 @@ public:
         seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
     }
 };
+
+
+
 
 #define ll long long
 
